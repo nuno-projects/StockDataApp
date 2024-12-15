2f458163e0bfbedf322ca5bc0fb127c3d3d7dec4
nuno-21902803 <nunomaluko5@gmail.com>
2024-12-15 15:42:45 +0000

    Initial Commit, Spring boot setup

---
+++ StockPriceData/src/main/java/com/tibss/StockPriceData/StockPriceDataApplication.java
@@ -0,0 +1,13 @@
+package com.tibss.StockPriceData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceDataApplication.class, args);
+	}
+
+}
---
+++ StockPriceData/src/test/java/com/tibss/StockPriceData/StockPriceDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibss.StockPriceData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
